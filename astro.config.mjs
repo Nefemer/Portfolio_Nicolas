9ea8e0f6319f90ff9ca66c62aa7efd25e72e95f7
Nefemer <nicofacumantaras@gmail.com>
2026-05-17 21:24:38 -0300

    fix: ruta de imagen para github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
-import { defineConfig } from 'astro/config';
+/*import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://Nefemer.github.io',
   base: '/portfolio_1',
+});
+*/
+
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://nefemer.github.io',
+  base: '/Portfolio_Nicolas',
 });
